32503afbd676405bac8c67fd01c7184d8b5b8b4c
root <root@Arrocero.localdomain>
2016-10-23 00:46:56 -0300

    gilada 1

---
+++ yummy.scm
@@ -0,0 +1,25 @@
+(define (cubito)
+    ;    (hint-none)
+    ;    (hint-wire)
+    (with-state
+        (colour (rndvec))
+        (build-cube))
+    )
+(clear)
+(define (cubitos c)
+    (cond ((not(zero? c))
+            (rotate (rndvec))
+            (translate (vector (tan (time)) 8 (cos(time))))
+            (cubito)
+            (cubitos (- c 1))
+            )
+        ))
+
+(define (render)
+    ;    (ortho)
+    ;    (set-ortho-zoom 55)
+    (clip 1 10000) ; MUEVE LA CAMARA
+    (rotate (vector (* 3 (sin(time))) (* 30 (cos(time))) 5 ))
+    (cubitos 10))
+
+(every-frame (render))
